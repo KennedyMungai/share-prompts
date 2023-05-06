1f82a589da6033b1daa97c4894a1cbce2956cc88
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-06 12:27:40 +0300

    Added the Logo to the Nav component

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+'use client'
+
+import Image from 'next/image'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Nav = () => {
-	return <div>Nav</div>
+	return (
+		<nav className='flex-between w-full mb-16 pt-3'>
+			<Link href={'/'} className='flex gap-2 flex-center'>
+				<Image
+					src={'/assets/images/logo.svg'}
+					width={'40'}
+					height={'40'}
+				/>
+			</Link>
+		</nav>
+	)
 }
 
 export default Nav
