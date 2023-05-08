3873139cbe586d06ae5bd33edcceeb1a63e79db9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-08 14:59:30 +0300

    Added an import to connect to the db

--- app/api/auth/[...nextAuth]/route.js
+++ app/api/auth/[...nextAuth]/route.js
@@ -1,3 +1,4 @@
+import { connectToDB } from '@/utils/database'
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
@@ -9,7 +10,10 @@ const handler = NextAuth({
 		})
 	],
 	async session({ session }) {},
-	async signIn({ profile }) {}
+	async signIn({ profile }) {
+		try {
+		} catch (error) {}
+	}
 })
 
 export { handler as GET, handler as POST }
